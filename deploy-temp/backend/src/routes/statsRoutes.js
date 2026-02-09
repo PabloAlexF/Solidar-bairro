d1bc790a55e3ed8fe2505dfb5f82575c8faa0d14
Pablo Andrade <pablolexandrade@gmail.com>
2026-02-08 22:12:08 -0300

    remova

--- deploy-temp/backend/src/routes/statsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const statsController = require('../controllers/statsController');
-const { authenticateToken } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/neighborhood', authenticateToken, statsController.getNeighborhoodStats);
-
-module.exports = router;
--- deploy-temp/backend/test-email.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-node test-email-config.js
-pause
